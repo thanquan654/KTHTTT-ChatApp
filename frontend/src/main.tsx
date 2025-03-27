d238ed8a5eec3b65dc077176f562851fba8fa94a
Quan Than <quantv654@gmail.com>
2025-03-27 08:59:44 +0700

    feat: config frontend ui lib and route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<BrowserRouter>
+		<Routes>
+			<Route index element={<App />} />
+			<Route path="/chat/:chatId" element={<div>Chat</div>} />
+			<Route path="/login" element={<div>Login</div>} />
+			<Route path="/signup" element={<div>Sign Up</div>} />
+		</Routes>
+	</BrowserRouter>,
 )
